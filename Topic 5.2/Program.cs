8a85d578b07c9b25897ce2a88298235154d6ce03
362Sadie-F <princessmae.fleming@gmail.com>
2026-02-24 12:58:20 -0500

    drive

--- Topic 5.2/Program.cs
+++ Topic 5.2/Program.cs
@@ -6,6 +6,7 @@
         {
             double grade;
             string name;
+            int age;
 
             Console.WriteLine("What was your lowest grade last semester?");
             grade = Convert.ToDouble(Console.ReadLine());
@@ -16,6 +17,20 @@
             else
             {
                 Console.WriteLine("Uh oh- Better luck next year!");
+            }
+            Console.WriteLine("press ENTER to continue");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("You're planning to drive? How old are you?");
+            age = Convert.ToInt32(Console.ReadLine());
+            if (age <=16)
+            {
+                Console.WriteLine("Keep off the roads!");
+            }
+            else
+            {
+                Console.WriteLine("Be safe out there!");
             }
         }
     }
